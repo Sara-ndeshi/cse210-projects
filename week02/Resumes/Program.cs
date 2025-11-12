99f91e8e6d3b09a89698b898bd183e57bb6be910
Sara-ndeshi <sarafinandeshy@gmail.com>
2025-11-12 04:36:05 +0200

    Finished Resumes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Console.WriteLine("Hello World! This is the Resumes Project.");
+        //new job object
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //second job object
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //create Resume object
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        //add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Display full resume;
+        myResume.Display();
     }
 }
