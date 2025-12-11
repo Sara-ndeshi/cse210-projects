94a60cdb8c1fb83fa28b8e8309fbd54508c478f6
Sara-ndeshi <sarafinandeshy@gmail.com>
2025-12-11 21:23:56 +0200

    Finished ExerciseTracking

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();  // miles or km
+    public abstract double GetSpeed();     // mph or kph
+    public abstract double GetPace();      // min per mile or km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min) - " +
+               $"Distance {GetDistance():0.0}, " +
+               $"Speed {GetSpeed():0.0}, " +
+               $"Pace {GetPace():0.00}";
+    }
+}
